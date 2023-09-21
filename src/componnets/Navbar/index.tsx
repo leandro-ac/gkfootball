6cef5d1f31ee132cf8432cf59cc731abf15e682a
leandro-ac <leandro.alvaresc@gmail.com>
2023-09-21 01:06:14 -0300

    mudando a estética da navbar

--- src/componnets/Navbar/index.tsx
+++ src/componnets/Navbar/index.tsx
@@ -1,4 +1,4 @@
-import { HStack } from "@chakra-ui/react";
+import { HStack, Stack } from "@chakra-ui/react";
 import { LinkNavbar } from "../Links";
 import { Image } from "../../../gkfootball/src/componnets/Image";
 
@@ -8,17 +8,56 @@ export function Navbar(){
         bg={"yellow"}
         py={8}
         justify={"center"}
-        spacing={40}
+        spacing={16}
         position={'sticky'}
         top={0}
         >
-            <Image src={"/imagens/Logo.png"} alt={"logo"} width={120} height={40} />
+            <Image src={"/imagens/Logo.png"} alt={"logo"} width={160} height={40} />
+            <Stack
+            bg={"lightblack"}
+            px={16}
+            py={4}
+            borderRadius={"16px"}
+            textColor={"yellow"}
+            >
             <LinkNavbar href={""} text={"HOME"}/>
+            </Stack>
+            <Stack
+            bg={"lightblack"}
+            px={14}
+            py={4}
+            borderRadius={"16px"}
+            textColor={"yellow"}
+            >
             <LinkNavbar href={""} text={"CONTATO"}/>
+            </Stack>
+            <Stack
+            bg={"lightblack"}
+            px={12}
+            py={4}
+            borderRadius={"16px"}
+            textColor={"yellow"}
+            >
             <LinkNavbar href={""} text={"MEMBROS"}/>
+            </Stack>
+            <Stack
+            bg={"lightblack"}
+            px={8}
+            py={4}
+            borderRadius={"16px"}
+            textColor={"yellow"}
+            >
             <LinkNavbar href={""} text={"GERENCIAMENTO"}/>
+            </Stack>
+            <Stack
+            bg={"lightblack"}
+            px={16}
+            py={4}
+            borderRadius={"16px"}
+            textColor={"yellow"}
+            >
             <LinkNavbar href={""} text={"LOGIN"}/>
-
+            </Stack>
         </HStack>
     )
 }
