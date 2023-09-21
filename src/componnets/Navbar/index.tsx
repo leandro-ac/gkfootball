b934c8f70438a1d8740dee8dbe925021e9d70164
leandro-ac <leandro.alvaresc@gmail.com>
2023-09-20 21:42:08 -0300

    estilização inicial da navbar

--- src/componnets/Navbar/index.tsx
+++ src/componnets/Navbar/index.tsx
@@ -1,18 +1,23 @@
 import { HStack } from "@chakra-ui/react";
 import { LinkNavbar } from "../Links";
+import { Image } from "../../../gkfootball/src/componnets/Image";
 
 export function Navbar(){
     return(
         <HStack
         bg={"yellow"}
-        py={40}
+        py={8}
         justify={"center"}
+        spacing={40}
+        position={'sticky'}
+        top={0}
         >
-            <LinkNavbar href={""} text={"home"}/>
-            <LinkNavbar href={""} text={"contato"}/>
-            <LinkNavbar href={""} text={"membros"}/>
-            <LinkNavbar href={""} text={"gerenciamento"}/>
-            <LinkNavbar href={""} text={"login"}/>
+            <Image src={"/imagens/Logo.png"} alt={"logo"} width={120} height={40} />
+            <LinkNavbar href={""} text={"HOME"}/>
+            <LinkNavbar href={""} text={"CONTATO"}/>
+            <LinkNavbar href={""} text={"MEMBROS"}/>
+            <LinkNavbar href={""} text={"GERENCIAMENTO"}/>
+            <LinkNavbar href={""} text={"LOGIN"}/>
 
         </HStack>
     )
