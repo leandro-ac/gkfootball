43f1f534e7360929e017b207bddccbe320f1cec2
leandro-ac <leandro.alvaresc@gmail.com>
2023-09-22 18:50:55 -0300

    estrutura membros e index, falta integrar as API

---
+++ src/componnets/Table/index.tsx
@@ -0,0 +1,41 @@
+import { Stack, Table, TableContainer, Tbody, Td, Th, Thead, Tr } from "@chakra-ui/react";
+
+interface TableIndexProps {
+    nome: string
+    email: string
+    niver: string
+    cargo: string
+}
+
+export function TableIndex({ nome, email, niver, cargo }: TableIndexProps) {
+    return (
+        <Stack>
+            <TableContainer>
+                <Table variant='simple'>
+                    <Thead
+                        bg={"yellow"}
+                    >
+                        <Tr
+                        >
+                            <Th border={"1px solid #000"}>Nome</Th>
+                            <Th border={"1px solid #000"}>E-mail</Th>
+                            <Th border={"1px solid #000"}>Aniversário</Th>
+                            <Th border={"1px solid #000"}>Cargo</Th>
+                        </Tr>
+                    </Thead>
+                    <Tbody
+                        bg={"white"}
+                    >
+                        <Tr>
+                            <Td border={"1px solid #000"}>{nome}</Td>
+                            <Td border={"1px solid #000"}>{email}</Td>
+                            <Td border={"1px solid #000"}>{niver}</Td>
+                            <Td border={"1px solid #000"}>{cargo}</Td>
+                        </Tr>
+                    </Tbody>
+
+                </Table>
+            </TableContainer>
+        </Stack>
+    )
+}
---
+++ src/pages/membros.tsx
@@ -0,0 +1,9 @@
+import { TableIndex } from "@/componnets/Table";
+
+export default function Table() {
+    return (
+    <>
+        <TableIndex nome={"l"} email={"l"} niver={"l"} cargo={"l"}></TableIndex>
+    </>
+ )
+}
