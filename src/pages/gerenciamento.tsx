9d9e980774dcc63af4b1902b583ceaae26b82783
leandro-ac <leandro.alvaresc@gmail.com>
2023-09-25 15:44:47 -0300

    atribuindo gerenciamento sem api

--- src/pages/gerenciamento.tsx
+++ src/pages/gerenciamento.tsx
@@ -4,13 +4,13 @@ import { Stack } from "@chakra-ui/react";
 import { Navbar } from '@/componnets/Navbar'
 import { Footer } from '@/componnets/Footer'
 
-export default function Membros() {
+export default function Gerenciamento() {
     return (
     <Stack><Navbar/>  
       
         <Stack py={4} px={4}>
             <Stack py={4} align={"center"}>
-            <TitleServices title={"MEMBROS"} size={"36px"} color={"yellow"}></TitleServices>
+            <TitleServices title={"GERENCIAMENTO"} size={"36px"} color={"yellow"}></TitleServices>
             </Stack>
             <TableIndex nome={"LEANDRO ALVARES DE CARVALHO"} email={"leandro@code"} niver={"24/09/2000"} cargo={"Assessor Comercial"} ></TableIndex>
         </Stack>
